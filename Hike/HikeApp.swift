c089faf47a743d1c87b21814f39a6cd63317357a
MadeReihan <reihanmade@gmail.com>
2023-12-16 22:03:50 +0700

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by made reihan on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
