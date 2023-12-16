758e2f9faef8d36ff83c546dccd1db7556cdba2b
MadeReihan <reihanmade@gmail.com>
2023-12-16 23:29:50 +0700

    header component

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,13 +12,35 @@ struct CardView:View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack{
-                Circle().fill(
-                    LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
-                ).frame(width: 256, height: 256)
+            VStack {
                 
+                VStack(alignment:.leading) {
+                    HStack {
+                        Text("Hiking").fontWeight(.black).font(.system(size: 52)).foregroundStyle(
+                            LinearGradient(colors: [Color.customGrayLight,Color.customGrayMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        
+                        Spacer()
+                        
+                        Button{
+                            print("press")
+                        } label: {
+                            CustomButtonView()
+                        }
+                        
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.").multilineTextAlignment(.leading).italic().foregroundColor(.customGrayMedium)
+                }.padding(.horizontal,30)
                 
-                Image("image-1").resizable().scaledToFit()
+                ZStack{
+                    Circle().fill(
+                        LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    ).frame(width: 256, height: 256)
+                    
+                    
+                    Image("image-1").resizable().scaledToFit()
+                }
             }
         }.frame(width: 320, height: 570)
     }
---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by made reihan on 16/12/23.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack{
+            Circle().fill(
+                LinearGradient(colors: [.white,.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom)
+            )
+            
+            Circle().stroke(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom),lineWidth:4)
+            
+            Image(systemName: "figure.hiking").fontWeight(.black).font(.system(size: 30)).foregroundStyle(
+                LinearGradient(colors: [.customGrayLight,.customGrayMedium], startPoint: .top, endPoint: .bottom)
+            )
+            
+        }.frame(width: 58, height: 58)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
